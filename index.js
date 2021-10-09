2ce28c371f760ace325d0c376ba241004b331b0d
harshjohar <hpsj3112@gmail.com>
2021-10-09 16:46:15 +0530

    Initial setup, dependencies

---
+++ index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+const express = require('express');
+var cors = require('cors');
+
+connectToMongo();
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res)=> {
+    res.send("hello world!");
+})
+
+app.listen(process.env.PORT || 5000, ()=> {
+    console.log(`StuTea listening at https://localhost:5000/`);
+})

c10b0bdcea797b4cc4b63aa68fd5508926541278
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-06 09:49:36 +0530

    db uri

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb+srv://harshjohar:Harsh2002%23@johar.fc1ia.mongodb.net/StuTea"
+const mongoURI = "mongodb+srv://stutea:kzJGbAgEKGJddxbZ@project.inobc.mongodb.net/Stutea"
 
 // localhost for testing
-const mongoURI = "mongodb://localhost:27017/stutea?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" 
+// const mongoURI = "mongodb://localhost:27017/stutea?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" 
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=> {

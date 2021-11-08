a09adbe78b69f8c074b1bfc3fb19e64a8323b18c
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-08 06:52:16 +0530

    questions tags, date on page

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
-// import dateFormat from 'dateformat';
+import dateFormat from 'dateformat';
 export const QuestionCard = (props) => {
     const {question} = props;
-    // const date = question.timestamp;
+    const date = question.timestamp;
     return (
         <div>
             {question.question}
-            {/* {dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT")} */}
+            {dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
+            {question.tags.map((tag) => {
+                return (<>{tag}</>)
+            })}
         </div>
     )
 }

5b76d07b69065cb69e350176512dc678f0063ddb
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-11-12 20:27:14 +0530

    small bug in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,11 +46,11 @@ export const Navbar = () => {
                             Contact
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    {localStorage.getItem('token') && <li className="nav-item">
                         <Link className={`navbar-item ${location.pathname==='/profile'?"active":""}`} to="/profile">
                             Profile
                         </Link>
-                    </li>
+                    </li>}
                 </ul>
 
                 {!localStorage.getItem("token")?

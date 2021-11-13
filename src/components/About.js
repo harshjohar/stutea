f80c2399d43ab5f411336d86a5826496a3344b02
Dhruv Bansal <bansaldhruv1973@gmail.com>
2021-11-13 11:33:39 +0530

    about page unresposive

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,175 @@
-import React from 'react'
-
+import React from "react";
+import "../css/About.css";
+import imag from "../Assets/help.jpg";
 export const About = () => {
-    return (
-        <div>
-            ABOUT
+  return (
+    <div>
+      <br />
+      <br />
+      <div className="about-intro">
+        <div className="intro-left">
+          <div className="intro-img">
+            <img src={imag} alt="StuTea" />
+          </div>
         </div>
-    )
-}
+        <div className="intro-right">
+          <h1>Learning and Teaching together</h1>
+          <br />
+          <h2>Our Motto</h2>
+          <p>At stutea we believe in making every learner a teacher.</p>
+          <p>That is exactly what our name signifies! Stu-dent + te-acher.</p>
+          <p>
+            We a group of college students came up with the idea of providing
+            students with a platform wherein they can post their doubts and
+            solve other students’ doubts. This concept would result in quicker
+            doubt resolution along with simple solutions because the student
+            solving doubts will be having similar knowledge and resources!
+          </p>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="founder-intro">
+        <h2>More about Stu-Tea developers</h2>
+        <br />
+        <div className="founder-cards d-flex flex-wrap justify-content-between">
+          <div className="card">
+            <div>
+              <img src={imag} alt="StuTea" />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Harshpreet Johar</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <h4>Contact me</h4>
+              <div className="handles-about">
+                <a href="/">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                  <i className="fas fa-envelope-open-text"> </i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <img src={imag} alt="StuTea" />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Harshpreet Johar</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <h4>Contact me</h4>
+              <div className="handles-about">
+                <a href="/">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                  <i className="fas fa-envelope-open-text"> </i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <img src={imag} alt="StuTea" />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Harshpreet Johar</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <h4>Contact me</h4>
+              <div className="handles-about">
+                <a href="/">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                  <i className="fas fa-envelope-open-text"> </i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <img src={imag} alt="StuTea" />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Harshpreet Johar</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <h4>Contact me</h4>
+              <div className="handles-about">
+                <a href="/">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                  <i className="fas fa-envelope-open-text"> </i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <img src={imag} alt="StuTea" />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Harshpreet Johar</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <h4>Contact me</h4>
+              <div className="handles-about">
+                <a href="/">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                  <i className="fas fa-envelope-open-text"> </i>
+                </a>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};

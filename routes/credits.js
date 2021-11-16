bfac3c789a26a27f4a00c3319b86e3f8ada6abf4
Jaagrit Arora <jaagritarora.btcse20@pec.edu.in>
2021-11-16 13:47:56 +0530

    credits and feedback

--- routes/credits.js
+++ routes/credits.js
@@ -4,11 +4,11 @@ const {body, validationResult} = require("express-validator");
 var fetchuser=require('../middleware/fetchuser');
 const Answers=require('../models/Answers');
 const Credits = require('../models/Credits');
+const User = require('../models/User');
 
 // ROUTE 1 : transaction : POST "/api/credits/transaction". Login Required
-router.post('/transaction', fetchuser, [
-    body('answer', 'Cannot be empty').exists()
-], async(req, res)=> {
+router.post('/transaction', fetchuser,
+     async(req, res)=> {
     const {question, answer, user}=req.body;
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
@@ -17,22 +17,75 @@ router.post('/transaction', fetchuser, [
     try {
 
         const answer = await Answers.findOne({question});
-        if (answer && answer.rating>2)
+        const aaidee = answer.user;
+        const credit = await Credits.findOne({"user" : aaidee});
+        
+        if (credit==null)
         {
+            const initialise = new Credits({
+                user : answer.user,
+                credits: 1000
+            })
+            const savedNote = await initialise.save();
+            const star = answer.rating;
+            if (answer.rating>2)
+            {
+                const crediti = await Credits.findOne({"user" : aaidee});
+                const item = crediti.credits + 100;
+                const deduction = await Credits.findOneAndUpdate({"user" : aaidee},{
+                    $set : {
+                        credits : item
+                    }
+                })
 
-            const debited = await Credits.updateOne(req.user.id,{
-                $set : {
-                    credits: rating - 10 
-                }
-            });
-
-            const credited = await Credits.updateOne(answer.user.id,{
-                $set : {
-                    credits: rating + 10 
-                }
-            });
-            res.send("credits updated");
+            }
+            // res.json(savedNote);
         }
+
+        else
+        {
+            const star = answer.rating;
+            if (answer.rating>2)
+            {
+                const item = credit.credits + 100;
+                const deduction = await Credits.findOneAndUpdate({answer},{
+                    $set : {
+                        credits : item
+                    }
+                })
+
+            }
+        }
+        const debitee = req.user.id;
+        const debit = await Credits.findOne({"user": debitee});
+        if (debit==null)
+        {
+            const initialiser = new Credits({
+                user : req.user.id,
+                credits : 1000
+            })
+            const saved  = await initialiser.save();
+            const debiti = await Credits.findOne({"user": debitee});
+            const item2 = debiti.credits - 100;
+            const less = await Credits.findOneAndUpdate({"user": debitee},{
+                $set : {
+                    credits: item2
+                }
+            })
+        }
+
+        else
+        {
+            const debiti = await Credits.findOne({"user": debitee});
+            const item2 = debiti.credits - 100;
+            const less = await Credits.findOneAndUpdate({"user": debitee},{
+                $set : {
+                    credits: item2
+                }
+            })
+        }
+
+        res.send("credits updated");
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");

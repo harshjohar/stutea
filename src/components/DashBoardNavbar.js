a029d4ec036fc5fec2e21ee7a7ed9dd00d24e7a0
Kanika Kaur <kanikaur01@gmail.com>
2021-11-23 12:48:40 +0530

    nav kinda responsive

--- src/components/DashBoardNavbar.js
+++ src/components/DashBoardNavbar.js
@@ -34,63 +34,67 @@ export const DashBoardNavbar = () => {
             <div className="link-list">
                 <ul className="links">
                     <li className="link-list-item">
-                    <div className="nav-icon">
                             <Link className={`ll-item ${location.pathname==='/'?"active-nav":""}`} to="/">
+                    <div className="nav-icon">
                                 {location.pathname==='/' ? <img src={homeActive} alt="" className="icon"/>:<img src={home} alt="" className="icon"/>}
+                        </div>
                                 <div className={`nav-text${location.pathname==='/'?"-active":""}`}>Home</div>
                             </Link>
-                        </div>
                     </li>
                     {localStorage.getItem('token') && <li className="link-list-item">
-                    <div className="nav-icon">
                         <Link className={`ll-item ${location.pathname==='/profile'?"active-nav":""}`} to="/profile">
+                    <div className="nav-icon">
                         {location.pathname==='/profile' ? <img src={profileActive} alt="" className="icon"/>:<img src={profile} alt="" className="icon"/>}
+                        </div>
                         <div className={`nav-text${location.pathname==='/profile'?"-active":""}`}>
                             Profile
                         </div>
                         </Link>
-                        </div>
                     </li>}
                     <li className="link-list-item">
-                        <div className="nav-icon">
                             <Link className={`ll-item ${location.pathname==='/about'?"active-nav":""}`} to="/about">
+                        <div className="nav-icon">
                                 {location.pathname==='/about' ? <img src={aboutActive} alt="" className="icon"/>:<img src={about} alt="" className="icon"/>}
+                        </div>
                                 <div className={`nav-text${location.pathname==='/about'?"-active":""}`}>About</div>
                                     
                             </Link>
-                        </div>
                     </li>
                     <li className="link-list-item">
-                    <div className="nav-icon">
                         <Link className={`ll-item ${location.pathname==='/contact'?"active-nav":""}`} to="/contact">
+                    <div className="nav-icon">
                         {location.pathname==='/contact' ? <img src={contactActive} alt="" className="icon"/>:<img src={contact} alt="" className="icon"/>}
+                        </div>
                             <div className={`nav-text${location.pathname==='/contact'?"-active":""}`}>Contact</div>
                         </Link>
-                        </div>
                     </li>
                     {localStorage.getItem('token') && <li className="link-list-item">
-                    <div className="nav-icon">
                         <Link to="/shop" className={`ll-item ${location.pathname==='/shop'?"active-nav":""}`}>
+                    <div className="nav-icon">
                         {location.pathname==='/shop' ? <img src={cartActive} alt="" className="icon"/>:<img src={cart} alt="" className="icon"/>}
+                        </div>
                             <div className={`nav-text${location.pathname==='/shop'?"-active":""}`}>Shop</div>
                         </Link>
-                        </div>
                     </li>}
                     
                 </ul>
             </div>
             {localStorage.getItem('token') ? <div className="logout">
                     <button className="logout-btn" onClick={handleLogout}>
-                    <img src={logout} alt="" className="icon"/>Logout</button>
+                    <div className="nav-icon">
+                    <img src={logout} alt="" className="icon"/></div>
+                    <div className="nav-text-active">Logout</div></button>
             </div> : <div className="logout">
                 <Link className="ll-item"  to="/login" style={location.pathname==='/login'?{display: "none"}:{display: "inline"}}>
                     <button className="logout-btn">
-                    <img src={login} alt="" className="icon"/>
+                    <div className="nav-icon">
+                    <img src={login} alt="" className="icon"/></div>
                     <div className="nav-text-active">Login</div></button>
                 </Link>
                 <Link className="ll-item" to="/register" style={location.pathname==='/register'?{display: "none"}:{display: "inline"}}>
                     <button className="logout-btn">
-                    <img src={register} alt="" className="icon"/>
+                    <div className="nav-icon">
+                    <img src={register} alt="" className="icon"/></div>
                     <div className="nav-text-active">Register</div></button>
                 </Link>
             </div>}

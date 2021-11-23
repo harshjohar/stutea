47af6587f3706114871f9165138a4011d84fe149
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-23 18:06:21 +0530

    small changes

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -7,7 +7,7 @@ export const Notification = (props) => {
     const date = notif.timestamp ? notif.timestamp : "";
     return (
         <div className="notif">
-            {notif.type==="answered" ?<div> Someone answered your question, <Link className="notif-ques-link" to={`/view/${notif.recents}`}>click to view it</Link>{dateFormat(date, "mmmm dS, yyyy, h:MM TT")}</div> :
+            {notif.type==="answered" ?<div className="notif-text"> Someone answered your question, <Link className="notif-ques-link" to={`/view/${notif.recents}`}>click to view it</Link>{dateFormat(date, "mmmm dS, yyyy, h:MM TT")}</div> :
              "Your response to this question is accepted, credits added to your account."}
         </div>
     )

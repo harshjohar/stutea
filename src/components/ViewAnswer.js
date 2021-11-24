91b5c89bf29bfa90cb859809bca4de2b5a4d8876
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-24 12:47:15 +0530

    changes + transaction

--- src/components/ViewAnswer.js
+++ src/components/ViewAnswer.js
@@ -71,7 +71,7 @@ export const ViewAnswer = () => {
             )}
             <h3>Answer</h3>
             {answer.answer ? answer.answer : answer.error}
-            {answer.answer && !(answer.rating) && <Feedback question={question}/> }
+            {answer.answer && !(answer.rating) && <Feedback question={question} answer={answer}/> }
             {answer.rating!==0 && <div>stars: {answer.rating}</div>}
         </div>
     )

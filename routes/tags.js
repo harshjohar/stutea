eb90151eb9171b45794546fe83e6c9eb8c1b172b
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-26 02:09:44 +0530

    remove fav tags

--- routes/tags.js
+++ routes/tags.js
@@ -53,4 +53,39 @@ router.get("/get", fetchuser, async (req, res) => {
     }
 });
 
+// ROUTE 3 : delete from favourites : POST "/api/tags/remove". Login required
+router.post('/remove', fetchuser, async(req, res)=> {
+    const { tag } = req.body;
+    const userId = req.user.id;
+    try {
+        const entry = await Favourites.findOne({
+            user: userId,
+        });
+
+        if (!entry) {
+            const newEntry = new Favourites({
+                user: userId,
+                tags: [],
+            });
+            await newEntry.save();
+        } else {
+            await Favourites.findOneAndUpdate(
+                {
+                    user: userId,
+                },
+                {
+                    $pull: {
+                        tags: tag,
+                    },
+                }
+            );
+        }
+        res.send({
+            msg: "Favourite Tags added",
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
 module.exports = router;

9ba560cad76a6965af48e86d31f7b234381419a0
Harshpreet Singh Johar <hpsj3112@gmail.com>
2021-11-25 15:14:38 +0530

    settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -44,52 +44,54 @@ export const Settings = () => {
     }
     return (
         <div className="settings-main">
-            <h1>settings</h1>
-            <form>
-            <div>
-                <label htmlFor="first">First Name</label>
-                <input type="text" placeholder="Enter your username" name="first" id="first" onChange={onChange} value={userData.first}/>
+            <h2 className="register-form-head">settings</h2>
+            <form className="rform">
+            <div className="rform-div">
+                <label htmlFor="first" className="rform-label">First Name</label>
+                <input type="text" placeholder="Enter your username" className="rform-input"name="first" id="first" onChange={onChange} value={userData.first}/>
             </div>
-            <div>
-                <label htmlFor="last">Last Name</label>
-                <input type="text" placeholder="Enter your username" name="last" id="last" onChange={onChange} value={userData.last}/>
+            <div className="rform-div">
+                <label htmlFor="last" className="rform-label">Last Name</label>
+                <input type="text" placeholder="Enter your username" className="rform-input"name="last" id="last" onChange={onChange} value={userData.last}/>
             </div>
-            <div>
-                <label htmlFor="city">City</label>
-                <input type="text" placeholder="Enter your username" name="city" id="city" onChange={onChange} value={userData.city}/>
+            <div className="rform-div">
+                <label htmlFor="city" className="rform-label">City</label>
+                <input type="text" placeholder="Enter your username" className="rform-input"name="city" id="city" onChange={onChange} value={userData.city}/>
             </div>
             </form>
 
             <div className="dp-selection">
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637785561/pp2_mgz6at.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637785561/pp2_mgz6at.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637785561/pp2_mgz6at.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637785560/pp1_ybanrh.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637785560/pp1_ybanrh.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637785560/pp1_ybanrh.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637785559/pp3_kr4oxm.png" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637785559/pp3_kr4oxm.png" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637785559/pp3_kr4oxm.png")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637785363/daniel-bernard-b56KbmGkfWg-unsplash_uofcgp.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637785363/daniel-bernard-b56KbmGkfWg-unsplash_uofcgp.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637785363/daniel-bernard-b56KbmGkfWg-unsplash_uofcgp.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637785354/cdd20-c6QH1SXGS8M-unsplash_txu97p.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637785354/cdd20-c6QH1SXGS8M-unsplash_txu97p.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637785354/cdd20-c6QH1SXGS8M-unsplash_txu97p.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637703190/60111_ak19ro.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637703190/60111_ak19ro.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637703190/60111_ak19ro.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1637703179/create-minimalist-vector-art-for-dp-or-social-media-use_hml37n.jpg" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1637703179/create-minimalist-vector-art-for-dp-or-social-media-use_hml37n.jpg" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1637703179/create-minimalist-vector-art-for-dp-or-social-media-use_hml37n.jpg")}/>
                 </div>
-                <div className="dp-item">
+                <div className={`dp-item ${dp==="https://res.cloudinary.com/stutea/image/upload/v1636875706/sample_nw8mlw.png" ? "dp-selected":""}`}>
                     <img src="https://res.cloudinary.com/stutea/image/upload/v1636875706/sample_nw8mlw.png" alt="" onClick={()=>setDp("https://res.cloudinary.com/stutea/image/upload/v1636875706/sample_nw8mlw.png")}/>
                 </div>
             </div>
+            <div className="form-button">
 
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit} className="form-submit-btn">
                 Submit
             </button>
+            </div>
         </div>
     )
 }

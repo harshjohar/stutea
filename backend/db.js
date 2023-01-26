5fb97bc805279fcc293d96b424165d94b29959bc
harsh johar <hpsj3112@gmail.com>
2023-01-26 14:01:18 +0530

    bcrypt -> bcryptjs

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const mongoURI = process.env.MONGO_URI;
-
-// localhost for testing
-// const mongoURI = "mongodb://localhost:27017/stutea?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" 
+const mongoose = require("mongoose");
+require("dotenv").config();
+const localMongo =
+    "mongodb://localhost:27017/stutea?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = process.env.MONGO_URI || localMongo;
 
 const connectToMongo = () => {
     mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI, ()=> {
+    mongoose.connect(mongoURI, () => {
         console.log("connected to mongo");
-    })
-}
+    });
+};
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -15,33 +15,44 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"), (req, res) => {
     res.send("authorization endpoint ready!, Frontend Not ready Yet");
 });
+
 app.use("/api/questions", require("./routes/questions"), (req, res) => {
     res.send("questions endpoint ready!, Frontend Not ready Yet");
 });
+
 app.use("/api/user", require("./routes/user"), (req, res) => {
     res.send("user endpoint ready!");
 });
+
 app.use("/api/answers", require("./routes/answers"), (req, res) => {
     res.send("answers endpoint ready");
 });
+
 app.use("/api/feedback", require("./routes/feedback"), (req, res) => {
     res.send("feedback endpoint ready");
 });
+
 app.use("/api/credits", require("./routes/credits"), (req, res) => {
     res.send("credits endpoint ready");
 });
+
+
 app.use("/api/shop", require("./routes/shop"), (req, res) => {
     res.send("shop endpoint ready");
 });
+
 app.use("/api/notifications", require("./routes/notifications"), (req, res) => {
     res.send("notifications endpoint ready");
 });
+
 app.use("/api/tags", require("./routes/tags"), (req, res) => {
     res.send("Tags endpoint ready!");
 });
+
 app.get("/", (req, res) => {
     res.send("hello world!, stutea here!");
 });
+
 app.get("/confirmation/:email/:token", confirmEmail);
 app.post("/resend", resendEmail);
 

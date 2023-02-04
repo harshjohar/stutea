21906dae126f69483303b926f800154dc2c28775
evirusky <evasabater17@gmail.com>
2023-02-04 22:04:29 +0100

    removed node modules , added by accident

--- node_modules/loading-animations-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import { Dots } from './components/Dots';
-import { MinimalSpinner } from './components/MinimalSpinner';
-import { ProgressBar } from './components/ProgressBar';
-import { Spinner } from './components/Spinner';
-import { TrinitySpinner } from './components/TrinitySpinner';
-import { Waves } from './components/Waves';
-
-export { Dots, MinimalSpinner, ProgressBar, Spinner, TrinitySpinner, Waves };
